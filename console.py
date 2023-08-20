9fe38f493d334d4cf7920b012d2bd193d1567381
codewithLevis <levisomondi368@gmail.com>
2023-08-20 07:32:07 -0700

    Settings

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -113,16 +113,38 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, args):
+    def do_create(self, arg):
         """ Create an object of any class"""
-        if not args:
+        if not arg:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+
+        args = arg.split(' ')
+        if args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+
+        args_dic = {}
+
+        for i in range(1, len(args)):
+            k, v = args[i].split('=')
+            if '.' in v:
+                try:
+                    v = float(v)
+                except ValueError:
+                    v = v.replace('"', '').replace('_', ' ')
+            else:
+                try:
+                    v = int(v)
+                except ValueError:
+                    v = v.replace('"', '').replace('_', ' ')
+            args_dic.update({k: v})
+
+        new_instance = HBNBCommand.classes[args[0]]()
+
+        for k, v in args_dic.items():
+            new_instance.__setattr__(k, v)
+
         print(new_instance.id)
         storage.save()
 
@@ -211,7 +233,7 @@ class HBNBCommand(cmd.Cmd):
                     print_list.append(str(v))
         else:
             for k, v in storage._FileStorage__objects.items():
-                print_list.append(str(v))
+                print_list.append(v)
 
         print(print_list)
 
@@ -272,7 +294,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +302,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -319,6 +341,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,6 +2,7 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
+from console import HBNBCommand
 
 
 class test_state(test_basemodel):
@@ -12,6 +13,14 @@ class test_state(test_basemodel):
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
+
+    def test_create_without_attributes(self):
+        new_instance = HBNBCommand()
+        new_instance.onecmd("create State")
+
+    def test_create_with_attributes(self):
+        new_instance = HBNBCommand()
+        new_instance.onecmd("create State name=\"California\"")
 
     def test_name3(self):
         """ """
